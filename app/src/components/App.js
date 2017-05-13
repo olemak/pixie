81f073af7a71ee8c0439a575ea78d2420bb90ef1
Ole Martin Kristiansen <olemak@apasje.no>
2017-05-13 11:21:38 +0200

    Basic setup
    Created the React app with Create-React-App, deleted and moved things
    around a bit.

---
+++ app/src/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h2>Header</h2>
+        </header>
+
+        <main>
+          <h2>main</h2>
+        </main>
+
+        <footer>
+          <h2>footer</h2>
+        </footer>
+
+      </div>
+    );
+  }
+}
+
+export default App;
