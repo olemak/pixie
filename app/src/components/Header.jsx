438d98abb11fb292d1560af07259362bb437eac7
Ole Martin Kristiansen <olemak@apasje.no>
2017-05-13 23:53:56 +0200

    ModelSubMenu renamed

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -4,9 +4,9 @@ import { Link, Route } from "react-router-dom";
 import "./Header.Style.css";
 import logo from "../logo.svg";
 
-const Bike = ({ match }) => (
-  <div>
-    <h2>Bike: {match.params.id}</h2>
+const ModelSubMenu = ({ match }) => (
+  <div className="Header Model__SubMenu">
+    <h2>Model: {match.params.id}</h2>
   </div>
 );
 
@@ -14,39 +14,39 @@ class Header extends Component {
   render() {
     return (
       <header className="Header">
-          <nav className="Header__navigation">
-            <ul className="Header__navigation__list">
-              <li className="Header__navigation__list__first">
-                <ul>
-                  <li>News</li>
-                  <li>Vision</li>
-                  <li>Racing</li>
-                  <li>Something</li>
-                </ul>
-              </li>
+        <nav className="Header__navigation">
+          <ul className="Header__navigation__list">
+            <li className="Header__navigation__list__first">
+              <ul>
+                <li>News</li>
+                <li>Vision</li>
+                <li>Racing</li>
+                <li>Something</li>
+              </ul>
+            </li>
 
-              <li className="Header__navigation__list__second">
-                <ul>
-                  <li>Service</li>
-                  <li>Find dealer</li>
-                  <li>Country (CUR)</li>
-                  <li>Wishlist</li>
-                </ul>
-              </li>
+            <li className="Header__navigation__list__second">
+              <ul>
+                <li>Service</li>
+                <li>Find dealer</li>
+                <li>Country (CUR)</li>
+                <li>Wishlist</li>
+              </ul>
+            </li>
 
-              <li className="Header__navigation__models">
-                <ul>
-                      <li><Link to="/mx">MX</Link></li>
-                      <li><Link to="/supermoto">Supermoto</Link></li>
-                      <li><Link to="/enduro">Enduro</Link></li>
-                </ul>
-              </li>
-            </ul>
-            <div className="menu__models__dropdown">
-              <Route path="/:id" component={Bike} />
-            </div>
-          </nav>
+            <li className="Header__navigation__models">
+              <ul>
+                <li><Link to="/mx">MX</Link></li>
+                <li><Link to="/supermoto">Supermoto</Link></li>
+                <li><Link to="/enduro">Enduro</Link></li>
+              </ul>
+            </li>
+          </ul>
+
+        </nav>
         <img className="Header__logo" src={logo} alt="KTM logo" />
+
+        <Route path="/:id" component={ModelSubMenu} />
 
       </header>
     );
